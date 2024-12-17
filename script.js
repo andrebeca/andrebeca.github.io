9f86a68e955d37cd787617f14aed288cad268ce4
Rebeca Ellen <becka336@gmail.com>
2024-12-17 01:42:56 -0300

    inclusão de galeria

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ $('.toggle-menu').click(function(){
     $(this).toggleClass('active');
     $('#menu-hamburguer').toggleClass('open');
 });
+
+let index = 0;
+
+function moveSlide(direction) {
+    const images = document.querySelectorAll('.carousel-images img');
+    const totalImages = images.length;
+
+    index = (index + direction + totalImages) % totalImages;
+
+    const newTransformValue = -index * 100;
+    document.querySelector('.carousel-images').style.transform = `translateX(${newTransformValue}%)`;
+}
